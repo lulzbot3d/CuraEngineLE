f5cc1c5289542f0befd83b3afef9c085891f174b
sfjelstad <sfjelstad@lulzbot.com>
2024-09-04 22:28:49 -0500

    Small change to conanfile to fix sentry-cli section

--- conanfile.py
+++ conanfile.py
@@ -218,7 +218,7 @@ class CuraEngineLEConan(ConanFile):
                 # create a sentry release and link it to the commit this is based upon
                 self.output.info(f"Creating a new release {self.version} in Sentry and linking it to the current commit {self.conan_data['commit']}")
                 self.run(f"sentry-cli --auth-token {os.environ['SENTRY_TOKEN']} releases new -o {sentry_org} -p {sentry_project} {self.version}")
-                self.run(f"sentry-cli --auth-token {os.environ['SENTRY_TOKEN']} releases set-commits -o {sentry_org} -p {sentry_project} --commit \"LulzBot/CuraEngineLE@{self.conan_data['commit']}\" {self.version}")
+                self.run(f"sentry-cli --auth-token {os.environ['SENTRY_TOKEN']} releases set-commits -o {sentry_org} -p {sentry_project} --commit \"lulzbot3d/CuraEngineLE@{self.conan_data['commit']}\" {self.version}")
                 self.run(f"sentry-cli --auth-token {os.environ['SENTRY_TOKEN']} releases finalize -o {sentry_org} -p {sentry_project} {self.version}")
 
     def deploy(self):
